b77f648e74b1bf175c85d718c1af88196ef96f4f
ashes999 <ashes999@yahoo.com>
2016-05-21 00:20:51 -0400

    Adhaan and muadhin, 5x/day at prayer times

--- Scripts/main.rb
+++ Scripts/main.rb
@@ -9,6 +9,8 @@ require 'Scripts/extensions/points_system_game_over_screen'
 require 'Scripts/extensions/advanced_game_time'
 require 'Scripts/extensions/script_event_page_conditions'
 require 'Scripts/extensions/swimming'
+require 'Scripts/extensions/lemony_sounds'
+require 'Scripts/extensions/lemonys_current_fps'
 
 DEFAULT_ACHIEVEMENTS = [
     Achievement.new("Khalid bin Walid", "Survive ten battles", "Placeholder achievement"),
@@ -27,8 +29,17 @@ class AdaanV3
   end
   
   def self.is_drowned?
-	Logger.log("HI! #{$game_variables[STARTED_SWIMMING_VARIABLE]} variables");
-	# 0 is the default value for variables that we didn't use yet.
-	return $game_variables[STARTED_SWIMMING_VARIABLE] != 0 && Time.new - $game_variables[STARTED_SWIMMING_VARIABLE] >= DROWN_AFTER_SECONDS
+    Logger.log("HI! #{$game_variables[STARTED_SWIMMING_VARIABLE]} variables");
+    # 0 is the default value for variables that we didn't use yet.
+    return $game_variables[STARTED_SWIMMING_VARIABLE] != 0 && Time.new - $game_variables[STARTED_SWIMMING_VARIABLE] >= DROWN_AFTER_SECONDS
+  end
+  
+  def self.is_salah_time?    
+    return true if GameTime.hour? == 5 && GameTime.min? >= 30 && GameTime.min? <= 39 # Fajr
+    return true if GameTime.hour? == 13 && GameTime.min? >= 15 &&  GameTime.min? <= 24 # Dhur    
+    return true if GameTime.hour? == 17 && GameTime.min? >= 20 && GameTime.min? <= 29 # Asr
+    return true if GameTime.hour? == 20 && GameTime.min? >= 43 && GameTime.min? <= 53 # Maghrib
+    return true if GameTime.hour? == 22 && GameTime.min? >= 25 && GameTime.min? <= 34 # Isha
+    return false
   end
 end
