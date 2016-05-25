b7d7be3f4b64aff802fd49e905baa3ebafc7066a
Haris1112 <hk12@live.ca>
2016-05-25 08:06:06 -0500

    Added MenuChanger.rb for custom in-game menu options

---
+++ Scripts/MenuChanger.rb
@@ -0,0 +1,37 @@
+#==============================================================================
+# ** MenuChanger
+#------------------------------------------------------------------------------
+#  This class modifies the in-game menu screen.
+#  -- ADDS "Achievements" option
+#==============================================================================
+
+class Scene_Menu < Scene_MenuBase
+  #--------------------------------------------------------------------------
+  # * Monkey-patch the "Create Command Window" method.
+  #--------------------------------------------------------------------------
+  alias_method :original_create_command_window, :create_command_window
+  def create_command_window
+    original_create_command_window
+
+    @command_window.set_handler(:achievements, method(:call_achievement_scene))
+  end
+
+  #--------------------------------------------------------------------------
+  # * Custom Menu Methods
+  #--------------------------------------------------------------------------
+  def call_achievement_scene
+    SceneManager.call(AchievementsScene)
+  end
+end
+
+class Window_MenuCommand < Window_Command
+  #--------------------------------------------------------------------------
+  # * Monkey-patch the "Custom Original Commands" method.
+  #--------------------------------------------------------------------------
+  alias_method :old_add_original_commands, :add_original_commands
+  def add_original_commands
+    old_add_original_commands
+
+    add_command("Achievements", :achievements, true)
+  end
+end
