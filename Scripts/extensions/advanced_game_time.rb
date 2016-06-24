7f14b5d9635016a0a7654469be66d538e6c31007
ashes999 <ashes999@yahoo.com>
2016-06-24 17:55:48 -0400

    Slightly slow down game time

--- Scripts/extensions/advanced_game_time.rb
+++ Scripts/extensions/advanced_game_time.rb
@@ -142,7 +142,7 @@ BATTLE_TINT = false
  
 #Time it takes for a second (or minute) to pass, in frames by default
 #(Frame rate is 60 frames per second)
-DEFAULT_TIMELAPSE = 30
+DEFAULT_TIMELAPSE = 45
 #Variable ID containing the current speed of time!
 TIMELAPSE_VARIABLE = 0
 #Whether to use seconds or not
