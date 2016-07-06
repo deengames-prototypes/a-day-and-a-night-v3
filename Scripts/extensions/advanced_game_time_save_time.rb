fc120df0bc6f0e87f76830d1fde2285897965f25
ashes999 <ashes999@yahoo.com>
2016-07-06 02:53:40 -0400

    Save game shows current time in-game

---
+++ Scripts/extensions/advanced_game_time_save_time.rb
@@ -0,0 +1,35 @@
+# For use with Advanced Game Time; the save game screen shows the current time
+# instead of the total gameplay time. Only shows hours and minutes, because
+# my game takes place over 24 hours.
+# Author: Ashes999 (ashes999@yahoo.com)
+# Version: 1.0
+
+class Window_SaveFile
+  def draw_playtime(x, y, width, align)
+      #header = DataManager.load_header(@file_index)
+      #return unless header
+      #Logger.log("Ahoy: #{header}")
+      
+      contents = {}
+      
+      filename = DataManager.make_filename(@file_index)
+      
+      if File.exist?(filename)
+        File.open(filename, "rb") do |file|
+          Marshal.load(file)
+          contents = Marshal.load(file)
+        end
+        
+        game_time = contents[:gametime]
+        hour = game_time.hour
+        am_pm = hour < 12 ? "AM" : "PM"
+        hour -= 12 if hour > 12
+        
+        minute = game_time.min
+        minute = "0#{minute}" if (minute < 10)
+        game_time_string = "#{hour}:#{minute} #{am_pm}"
+        
+        draw_text(x, y, width, line_height, game_time_string, 2)
+      end
+  end
+end
