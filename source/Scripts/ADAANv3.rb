e82bddb6e2866d66a8a9db06b8a3f7cc3413faca
ashes999 <ashes999@yahoo.com>
2016-11-20 14:31:10 -0500

    Fixed faces, I hope, for good.

--- source/Scripts/ADAANv3.rb
+++ source/Scripts/ADAANv3.rb
@@ -181,7 +181,7 @@ class AdaanV3
     return 'Fajr' if GameTime.hour? == 5 && GameTime.min? >= 30 && GameTime.min? <= 40
     return 'Dhur' if GameTime.hour? == 13 && GameTime.min? >= 15 &&  GameTime.min? <= 25
     return 'Asr' if GameTime.hour? == 17 && GameTime.min? >= 20 && GameTime.min? <= 30
-    return 'Maghrib' if GameTime.hour? == 20 && GameTime.min? >= 43 && GameTime.min? <= 53
+    return 'Maghrib' if GameTime.hour? == 20 && GameTime.min? >= 45 && GameTime.min? <= 55
     return 'Isha' if GameTime.hour? == 22 && GameTime.min? >= 25 && GameTime.min? <= 35
     return nil
   end
